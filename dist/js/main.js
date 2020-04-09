9f5da3fb4e8297d1b11db57649a23eb0a71df4f5
Li <liyunwei10@gmail.com>
2020-04-08 20:51:41 -0400

    menu show/hide logic

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+const menu = document.querySelector(".menu");
+const menuButton = document.querySelector(".menu-button");
+const menuProfile = document.querySelector(".menu-profile");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-items");
+
+// initial state of menu
+let showMenu = false;
+
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuButton.classList.add("closed");
+    menu.classList.add("show");
+    menuProfile.classList.add("show");
+    menuNav.classList.add("show");
+    navItems.forEach(item => {
+      item.classList.add("show");
+    });
+    showMenu = true;
+  } else {
+    menuButton.classList.remove("closed");
+    menu.classList.remove("show");
+    menuProfile.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach(item => {
+      item.classList.remove("show");
+    });
+    showMenu = false;
+  }
+}
